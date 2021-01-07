e9de31ea0c7f4508bc76127ca85b378f469150e6
Anthony Dellinger <adell892@live.kutztown.edu>
2021-01-07 17:45:26 -0500

    added graphs

--- analysis/ssit.jl
+++ analysis/ssit.jl
@@ -7,19 +7,24 @@ table = XLSX.readtable(
     "method_problem_results")
 df = DataFrame(table...)
 
+theme() = Theme(
+    panel_stroke=colorant"black",
+    background_color=colorant"white",
+    # point_size=.07cm,
+    grid_color=colorant"grey")
+
 function save_plot(p, name)
-    Gadfly.with_theme(:dark) do
-        img = PNG("analysis/graphs/$name.png", 30cm, 10cm, dpi=300)
-        draw(img, p)
-    end
+    img = PNG("analysis/graphs/$name.png", 30cm, 35cm, dpi=300)
+    draw(img, p)
 end
 
-feasible_df = df |> @filter(_.infeasibility == 0 && _.case > 3 && _.dataset > 6) |> DataFrame
+feasible_df = df |> @filter(_.infeasibility == 0 && _.dataset > 4) |> DataFrame
 
 xticks = [.005, .05, .25, .5, .75]
 p = plot(feasible_df, x="lowest_gap", y="total_time",
     ygroup="case", xgroup="dataset",
     color="method_name",
     # Scale.x_log10(),
-    Geom.subplot_grid(Geom.point))
+    Geom.subplot_grid(Geom.point),
+    theme())
 save_plot(p, "scatter")
